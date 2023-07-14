57586e30544c9a9bb9ebee2213a8618a8c5b040a
masifmirza927 <masifmirza927@gmail.com>
2023-07-14 15:45:25 +0500

    updates

--- src/components/inputfield/InputField.jsx
+++ src/components/inputfield/InputField.jsx
@@ -4,13 +4,19 @@ import { Link } from 'react-router-dom';
 
 
 function InputField() {
-    const [inputTxt, setInputTxt] = useState("");
+  const [inputTxt, setInputTxt] = useState("");
+  const [price, setPrice] = useState(null);
 
- 
   return (
-    <div>
-        <input onChange={ (e) => setInputTxt(e.target.value)} type='text' className='inputField' />
-        <Link to={`/search?q=${inputTxt}`}  className='btn  btn-dark' style={{ width: '140px' }}>Search</Link>
+    <div className='inputFiledBox'>
+      <input onChange={(e) => setInputTxt(e.target.value)} type='text' className='inputField' />
+      <select onChange={(e) => setPrice(e.target.value)} class="form-select" style={{ width: "20%" }}>
+        <option selected value="0">Select Price</option>
+        <option value="10000-20000">10000 - 20000</option>
+        <option value="21000-30000">21000 - 30000</option>
+        <option value="30000-">30000+</option>
+      </select>
+      <Link to={`/search?q=${inputTxt}&price=${price}`} className='btn  btn-dark' style={{ width: '140px' }}>Search</Link>
     </div>
   )
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -6,27 +6,35 @@ function Search() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [products, setProducts] = useState([]);
   const q = searchParams.get("q");
+  const price = searchParams.get("price");
+  const [isLoding, setIsLoading] = useState(false);
 
-console.log(q);
+  console.log(q);
 
-useEffect( () => {
-  axios.get("http://localhost:3003/search?title="+q).then( (res) => {
-      if(res.data.status == true) {
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get(`http://localhost:3003/search?q=${q}&price=${price}`).then((res) => {
+      if (res.data.status == true) {
         setProducts(res.data.products);
+        setIsLoading(false);
       }
-  })
-}, [searchParams]);
+    }).catch(err => { setIsLoading(false); console.log(err.message) })
+  }, [searchParams]);
 
   return (
-    <div>
-      {
-        products.map( (product) => {
-          return (<>
-              <h1>{product.title}</h1>
-          </>)
-        })
+    (isLoding) ? <div class="spinner-border text-danger" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div> : (products.length > 0) ? <div>
+      {products.map((product) => {
+        return (<>
+          <div style={{ backgroundColor: "#efefef", padding: "20px", margin: "10px" }}>
+            <h1>{product.title}</h1>
+            <h3>{product.price}</h3>
+          </div>
+        </>)
+      })
       }
-    </div>
+    </div> : <div class="alert alert-danger" role="alert">0 mobile found</div>
   )
 }
 
